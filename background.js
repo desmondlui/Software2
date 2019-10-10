fa6b8e55dd258fe1e94a681d4bc50aeea25327c8
De3trvctions <35520928+De3trvctions@users.noreply.github.com>
2019-10-10 09:50:17 +0800

    Add files via upload

---
+++ background.js
@@ -0,0 +1,39 @@
+(async function () {
+    "use strict";
+
+    //TODO: Hide if the default search engine is already google?
+    //There's no listener methods available for browser.search so right now it would need to be loop based, which isn't great.
+    //async function searchCheck() {
+    //    let search = (await browser.search.get()).find(_ => _.isDefault).name;
+    //    if (search === "Google") //Can it ever be called something else?
+    //        ;//Remove context menu item
+    //    else
+    //        ;//Add context menu item
+    //}
+    //var interval = setInterval(searchCheck, 1000);
+
+    browser.menus.create({
+        id: "search-google",
+        title: "Search &Google for \"%s\"",
+        contexts: ["selection"]
+    });
+
+    browser.menus.onClicked.addListener(async (info, tab) => {
+        if (info.menuItemId !== "search-google" || !info.selectionText)
+            return;
+
+        let index;
+        let newTabPosition = await browser.browserSettings.newTabPosition.get({});
+        if (newTabPosition.value !== "atEnd") {
+            index = tab.index + 1;
+        }
+
+        let url = new URL("https://www.google.com/search");
+        url.searchParams.set("q", info.selectionText.trim());
+
+        await browser.tabs.create({
+            index: index,
+            url: url.href
+        });
+    });
+}());
